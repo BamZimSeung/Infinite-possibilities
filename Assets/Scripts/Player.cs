49431fabbbc0cfa1310304477989f2cd34cb763b
Ju Sung Park <pkjs91@naver.com>
2017-09-03 22:02:39 +0900

    Start

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerControl))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+
+    Camera viewCamera;
+    PlayerControl controller;
+    GunController gunController;
+
+	void Start () {
+        controller = GetComponent<PlayerControl>();
+        gunController = GetComponent<GunController>();
+        viewCamera = Camera.main;
+	}
+	
+	void Update () {
+        // 이동 입력
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        // 바라보기 입력
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            // Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
+
+        // 무기 입력
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerControl : MonoBehaviour {
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookAtPoint)
+    {
+        Vector3 hightCorrectedPoint = new Vector3(lookAtPoint.x, transform.position.y, lookAtPoint.z);
+        transform.LookAt(hightCorrectedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
